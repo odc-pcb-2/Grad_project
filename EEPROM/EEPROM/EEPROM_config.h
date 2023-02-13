590fdd5e0fe092ab9e4faca8374f916def727754
Ahmed Emad Elsherbini Ali <66177429+AhmedEmad2001@users.noreply.github.com>
2023-02-13 03:59:48 +0200

    Delete EEPROM directory

--- EEPROM/EEPROM/EEPROM_config.h
+++
@@ -1,5 +0,0 @@
-#ifndef _MCAL_EEPROM_CONFIG_H_
-#define _MCAL_EEPROM_CONFIG_H_
-
-
-#endif
--- EEPROM/EEPROM/smart_lock.h
+++
@@ -1,8 +0,0 @@
-
-void start();
-void read_EEPROM();
-void write_EEPROM();
-void check_pass();
-void Scan_pass();
-u8 smart_lock();
-
